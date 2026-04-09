4577447d02f7504892d9e58eae68bb642e79e8da
lps29-max <lps29@aluno.ifnmg.edu.br>
2026-04-09 22:47:33 +0000

    feat: pratica fundamentos de JavaScript no Node - aula 05

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -7,25 +7,62 @@
 
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
-  // escreva seu código aqui
+    if (idade < 12) {
+    return "criança";
+  } else if (idade <= 17) {
+    return "adolescente";
+  } else {
+    return "adulto";
+  }
 }
+
+
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  // escreva seu código aqui
+   const desconto = preco * (percentual / 100);
+  return preco - desconto;
 }
+
+
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  // escreva seu código aqui
+   if (nota >= 60) {
+    return "aprovado";
+  } else if (nota >= 40) {
+    return "recuperação";
+  } else {
+    return "reprovado";
+  }
 }
+
+
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  // escreva seu código aqui
+   let soma = 0;
+
+  for (const numero of numeros) {
+    if (numero % 2 === 0) {
+      soma += numero;
+    }
+  }
+
+  return soma;
 }
+
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+   let contador = 0;
+
+  for (const char of texto) {
+    if (char === letra) {
+      contador++;
+    }
+  }
+
+  return contador;
 }
+
