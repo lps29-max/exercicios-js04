e2d32872bf89da7db4ba859351d86f4001b0d77d
lps29-max <lps29@aluno.ifnmg.edu.br>
2026-04-30 17:15:40 +0000

    feat: aula 08 concluída

--- exercicios/aula08/utils/texto.js
+++ exercicios/aula08/utils/texto.js
@@ -2,6 +2,8 @@
 // Aula 08 — Default Export
 // ========================================
 // Implemente a função e exporte com "export default".
+
 export default function formatarNome(nome) {
-  // escreva seu código aqui
+  return nome.charAt(0).toUpperCase() + 
+  nome.slice(1).toLowerCase()
 }
